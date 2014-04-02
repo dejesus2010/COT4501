986d8077d2fd00d8a757d6700dc60d56142c688c
dgollahon <dgollahon@users.noreply.github.com>
2014-04-01 23:05:22 -0400

    Bisection method first commit.

---
+++ Assignment6/Bisection.m
@@ -0,0 +1,11 @@
+function [root, convergence] = Bisection(a,b,f)
+  tol = 10e-10;
+  while((b - a) > tol)
+    m = a + (b - a)/2
+    if sign(f(a)) == sign(f(m))
+      a = m;
+    else
+      b = m;
+    end
+  end
+end

88f4e77c3d3dbf68965e40360f32af1d04bb985a
dgollahon <dgollahon@users.noreply.github.com>
2014-04-01 23:25:30 -0400

    bisection, it works! except does not measure convergence.

--- Assignment6/Bisection.m
+++ Assignment6/Bisection.m
@@ -1,11 +1,13 @@
 function [root, convergence] = Bisection(a,b,f)
   tol = 10e-10;
   while((b - a) > tol)
-    m = a + (b - a)/2
+    m = a + (b - a)/2;
     if sign(f(a)) == sign(f(m))
       a = m;
     else
       b = m;
     end
   end
+  root = m;
+  convergence = -11;
 end

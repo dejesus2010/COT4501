b523b0dcc02b8aca567c69921b339306b06e77e2
Ryan <dejesus2010@users.noreply.github.com>
2014-04-03 10:55:08 -0400

    Updated Newton.m to calculate convergence

--- Assignment6/Newton.m
+++ Assignment6/Newton.m
@@ -9,14 +9,26 @@ function [root, convergence] = Newton( f, df, x_guess, tol, maxIterations )
 
   %f = inline(f);
   %df = inline(df);
-  root(1) = x_guess - (f(x_guess)/df(x_guess));
-  convergence(1) = abs(root(1) - x_guess );
-  k = 2;
-  while ( convergence(k-1) >= tol ) && (k <= maxIterations)
-    root(k) = root(k-1) - (f(root(k-1))/df(root(k-1)));
-    convergence(k) = abs(root(k)-root(k-1));
+  root = x_guess;
+  
+  
+  root = [root;x_guess - (f(x_guess)/df(x_guess))];
+  convergence = abs(root(2,1) - x_guess );
+  x_guess = root(2,1);
+  rel_change = abs( root(2,1) - root(1,1) ) / root(1,1);
+  k = 3;
+  while ( rel_change >= tol ) && (k <= maxIterations)
+    root = [root;x_guess - (f(x_guess)/df(x_guess))];
+    convergence = [convergence; abs(root(k,1)-x_guess)];
+    rel_change = abs( root(k,1) - root(k-1,1) ) / root(k-1,1);
+    x_guess = root(k,1);
     k = k+1;
   end
 
+  [m,n] = size(convergence);
+  sum_of_errors = sum(convergence);
+  convergence_rate = sum_of_errors/convergence(1,1)  
+  
+  
 
 end %function%

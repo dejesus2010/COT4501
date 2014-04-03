261a8acbf4c0a9aef6fb53beb5b0d47cef3e3542
Ryan <dejesus2010@users.noreply.github.com>
2014-04-03 12:15:12 -0400

    Added computer problem and section
    Also, deleted exercises... to be included as images

--- Assignment6/HW_6_Script.m
+++ Assignment6/HW_6_Script.m
@@ -95,32 +95,61 @@
 %  (a) Bisection: 1 bit per iteration. (p. 226)
 %  (b) Newton's method: The number of correct digits is doubled at each iteration. (p. 231)
 
-%% Exercise 5.1
-%
-%
-
-%% Exercise 5.2
-%
-%
-
-%% Exercise 5.3
-%
-%
-
-%% Exercise 5.4
-%
-%
-
-%% Exercise 5.9
-%
-%
-
-%% Exercise 5.10
-%
-%
-
-%% Exercise 5.13
-%
-%
+%% Refer to images for the answers to the exercises
 
 %% Computer Problem 5.3
+a = @(x)x^3-2*x-5;
+da = @(x)3*x^2-2;
+b = @(x)exp(-x)-x;
+db = @(x)-1*exp(-x);
+c = @(x)x*sin(x)-1;
+dc = @(x)x*cos(x)+sin(x);
+d = @(x)x^3-3*x^2+3*x-1;
+dd = @(x)3*x^2-6*x+3;
+tol = 1e-8;
+maxIterations = 500;
+%% Computer Problem 5.3(a)
+% (a)
+disp('Bisection (a)');
+[root,convergence] = Bisection(2,4,a)
+disp('Newton (a)');
+[root,convergence,convergence_rate] = Newton(a,da,2,tol,maxIterations)
+disp('Secant (a)');
+[root,hx] = Secant(a,2,4,tol,maxIterations)
+disp('Library function (a)');
+[x,fval,exitflag] = fsolve(a,2)
+%% Computer Problem 5.3(b)
+% (b)
+disp('Bisection (b)');
+[root,convergence] = Bisection(0.2,1,b)
+disp('Newton (b)');
+[root,convergence,convergence_rate] = Newton(b,db,0.53,tol,maxIterations)
+disp('Secant (b)');
+[root,hx] = Secant(b,0.87,1,tol,maxIterations)
+disp('Library function (b)');
+[x,fval,exitflag] = fsolve(b,2)
+%% Computer Problem 5.3(c)
+% (c)
+disp('Bisection (c)');
+[root,convergence] = Bisection(2,3,c)
+disp('Newton (c)');
+[root,convergence,convergence_rate] = Newton(c,dc,3.1,tol,maxIterations)
+disp('Secant (c)');
+[root,hx] = Secant(c,1.8,3,tol,maxIterations)
+disp('Library function (c)');
+[x,fval,exitflag] = fsolve(c,3)
+%% Computer Problem 5.3(d)
+
+disp('Bisection (d)');
+[root,convergence] = Bisection(-6,7,d)
+disp('Newton (d)');
+[root,convergence,convergence_rate] = Newton(d,dd,1.03,tol,maxIterations)
+disp('Secant (d)');
+[root,hx] = Secant(d,2.3,1.03,tol,maxIterations)
+disp('Library function (d)');
+[x,fval,exitflag] = fsolve(d,223)
+
+
+
+
+

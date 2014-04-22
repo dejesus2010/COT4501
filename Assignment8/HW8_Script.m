1f24253aac940b9eb029f9b00e24cee1ac2d1747
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2014-04-22 17:03:00 -0400

    Completed everything except lagrange poly probs

--- Assignment8/HW8_Script.m
+++ Assignment8/HW8_Script.m
@@ -90,20 +90,131 @@
 %  at the  endpoints t1 and tn (if the spline is going to be periodic). [p. 327]
 
 %% Exercise 7.1 (skip part c)
-//work out
+
+%input data
+t = [-1 0 1]';
+y = [1 0 1]';
+
+%a)
+disp('a) Monomial Basis');
+A = [ones(3,1) t(:) t(:).^2]; %vandermonde matrix
+x = A \ y; %compute coefficients
+disp(['P2(t) = ' num2str(x(3)) '*t^2 + ' num2str(x(2)) '*t + ' num2str(x(1))]);
+
+%b)
+disp(' ');
+disp('b) Lagrange Basis');
+
+%calculate lagrange??
+
+%result
+disp(' ');
+disp('As shown above, both methods produce the same interpolating polynomial.');
 
 %% Exercise 7.4 (skip part c)
 %  a)
-%     Using Horner's method, only n multiplications (and n additions) need to 
-%     be performed. [p. 316]
+%     Using Horner's method, evaluating the polynomial represented in the monomial 
+%     basis requires only O(n) multiplications to be performed. [p. 316]
 %  b)
-%     2*n ?? 3*n ?? [p. 317]
+%     Evaluating the Lagrange polynomial requires O(n^2) multiplications. [p. 317]
 
 %% Exercise 7.5 (skip part c)
-//work out?
+%input data
+t = [1 2 3 4]';
+y = [11 29 65 125]';
+
+%a)
+disp('a) Monomial Basis');
+A = [ones(4,1) t(:) t(:).^2 t(:).^3]; %vandermonde matrix
+x = A \ y; %compute coefficients
+disp(['P2(t) = ' num2str(x(3)) '*t^2 + ' num2str(x(2)) '*t + ' num2str(x(1))]);
+
+%b)
+disp(' ');
+disp('b) Lagrange Basis');
+
+%calculate lagrange??
+
+%result
+disp(' ');
+disp('As shown above, both methods produce the same interpolating polynomial.');
 
 %% Computer Problem 7.4
+runge = @(t)(1./(1+25*t.^2));
+t11 = -1:.2:1;
+t21 = -1:.1:1;
+xplot = -1:.001:1;
+
+%%
+%  Polynomial interpolant: n=11
+fig1 = figure(1);
+
+p = polyfit(t11,runge(t11),10);%polynomial of degree n-1
+plot(xplot,polyval(p,xplot),'b-',t11,runge(t11),'ro');
+
+%%
+%  Cubic spline: n=11
+fig2 = figure(2);
+
+yy = spline(t11,runge(t11),xplot);
+plot(xplot,yy,'b-',t11,runge(t11),'ro');
+
+%%
+%  Polynomial interpolant: n=21
+fig3 = figure(3);
+
+p = polyfit(t21,runge(t21),20);%polynomial of degree n-1
+plot(xplot,polyval(p,xplot),'b-',t21,runge(t21),'ro');
+
+%%
+%  Cubic spline: n=21
+fig4=figure(4);
+
+yy = spline(t21,runge(t21),xplot);
+plot(xplot,yy,'b-',t21,runge(t21),'ro');
 
 
 %% Computer Problem 7.5
+%input data
+t = [0 0.5 1 6  7  9];
+y = [0 1.6 2 2 1.5 0];
 
+xplot = 0:.001:9;
+
+%%
+%  a) Polynomial Fit of Degree 5
+fig5=figure(5);
+
+p = polyfit(t,y,5);
+plot(xplot,polyval(p,xplot),'b-',t,y,'ro');
+
+%%
+%  b) Cubic Spline Fit
+fig6=figure(6);
+
+yy = spline(t,y,xplot);
+plot(xplot,yy,'b-',t,y,'ro');
+
+%%
+%  c)
+%  The spline seems to give the more reasonable values between data points.
+%  This is because the spline considers a continuous derivative and
+%  curvature across the points which smooths out the connecting curves, but
+%  the polyfit method only ensures that all the points are passed through 
+%  and does not attempt to moderate the derivatve at each point. More
+%  importantly, since the polyfit function is a continuous polynomial, it
+%  is required to have a dip either up or down between the two center
+%  points which share a y value and this introduces undesirable "wiggle".
+
+%% 
+%  d) Piecewise Linear Interpolation
+%  Yes, in this case a direct line fit would be more desirable. This is
+%  true because it tends to fit this particular set of data--especially
+%  since there are two consecutive points whose y values are the same...
+%  causing a straight line to be the most logical interpolation here. See
+%  the plot below. This however does come at the cost of jagged edges and a
+%  discontinuous derivative so the ideality of this approach would depend
+%  on the desired smoothness and the application the data is required for.
+
+fig7=figure(7);
+plot(t,y,'b-',t,y,'ro');

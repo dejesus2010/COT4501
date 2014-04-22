f2b2629991fcfb8c81939207dba8ca41d9d1ae48
dgollahon <dgollahon@users.noreply.github.com>
2014-04-22 01:37:26 -0400

    Create HW8_Script.m

---
+++ Assignment8/HW8_Script.m
@@ -0,0 +1,28 @@
+%% Numerical Analysis Homework Assignment 8
+%  Ryan Dejesus
+%  Daniel Gollahon
+%  Ron Quan
+
+%% Review 7.11
+
+%% Review 7.12
+
+%% Review 7.13
+
+%% Review 7.24
+
+%% Review 7.25
+
+%% Review 7.26
+
+%% Review 7.27
+
+%% Exercise 7.1 (skip part c)
+
+%% Exercise 7.4 (skip part c)
+
+%% Exercise 7.5 (skip part c)
+
+%% Computer Problem 7.4
+
+%% Computer Problem 7.5
